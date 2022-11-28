5300d44f04ba7a749c57d5e0668e92b9acd7a740
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-11-28 12:13:43 -0800

    Concatenating strings and mixing them

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ \nlanguage that combines remarkable power with very clear syntax"
-str = str[39:65] + str[107:114] + str[:6]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
