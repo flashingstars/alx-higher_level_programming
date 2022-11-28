6b58bbc1a630cc99d8d156cea4d155a32c28cfab
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-11-28 11:49:10 -0800

    Printing concatenated string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\ \nlanguage that combines remarkable power with very clear syntax"
-str = str[39:66] + str[107:113] + str[:6]
+str = str[39:65] + str[107:114] + str[:6]
 print(str)
