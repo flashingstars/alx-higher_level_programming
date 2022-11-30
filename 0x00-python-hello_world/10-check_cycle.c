1d506b01ade648da9dbb21a23d8e7abe9827e0ff
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-11-30 00:32:16 -0800

    Checking for loop in list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,27 @@
 #include <lists.h>
 
 /**
+ * check_cycle - checks for a cycle in a linked list
  *
+ * @list: head of the list
  *
+ * Return: 0 on success, 1 on failure
  */
 
+int check_cycle(list_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
 
+	if (list == NULL)
+		return (1);
+	while (slow->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (0);
+	}
+	return (0);
+}
