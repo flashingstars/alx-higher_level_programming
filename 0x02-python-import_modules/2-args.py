fa73f476992b26afbd418a6cb4739c8725521e1a
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-01 11:09:44 -0800

    Printing arguments to the screen

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    elif len(sys.argv) > 2:
+        j = 1
+        print("{} arguments:".format((len(sys.argv) - 1)))
+        for i in range(len(sys.argv[1:])):
+            print("{}: {:s}".format(j, sys.argv[j]))
+            j = j + 1
