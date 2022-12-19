c3797fd97cae7bfeec1cc6d392ceeccd020df6e0
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-19 03:49:45 -0800

    Printing a numbered list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     index = 0
-    for i in range (x):
+    for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
             length += 1
