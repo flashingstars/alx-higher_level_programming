fda749b53e0157d236308e91af1508c298162e3d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-20 10:56:51 -0800

    Initializing private square class with raises

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module that defines square"""
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing the square class
+        Argument is size. It represents the size of the square defined
+        Raises:
+        - TypeError if size is not an integer
+        - ValueError if size is less than 0
+        """
+
+        if not int(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
+
+        self.__size = size
