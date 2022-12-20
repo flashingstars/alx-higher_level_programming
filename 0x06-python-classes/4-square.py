2ed649c80447f4d0d2704cd8cc7af746605e02fd
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-20 11:53:54 -0800

    Retrieving and using setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""A module that defines a square"""
+
+
+class Square:
+    """Represents a square class"""
+
+    def __init__(self, size=0):
+        """Initializing the square class
+        Arguments is size. It represents the size of the square class
+        Raises:
+        - TypeError if size is not an integer
+        - ValueError if size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves size of a square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+        def area(self):
+        """
+        Calculates area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
