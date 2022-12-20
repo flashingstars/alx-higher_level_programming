1c69e8231f79bd8f844ddd127917134fc0fb84b3
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-20 03:39:48 -0800

    Printing a list and it's length with exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index = 0
+    count = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            length += 1
+            count += 1
         except IndexError:
             break
-    print()
-    return(index)
+    print("")
+    return(count)
