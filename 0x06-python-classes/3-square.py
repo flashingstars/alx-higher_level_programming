a24d5a4cb8dc6fb489d2906f1d430be2ef50300f
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-20 11:44:51 -0800

    Square of size

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,20 +5,20 @@
 class Square:
     """Represents a square class"""
 
-    def __init__(self, size=0)
-    """Initializing the square class
-    Arguments is size. It represents the size of the square class
-    Raises:
-    - TypeError if size is not an integer
-    - ValueError if size is less than 0
-    """
+    def __init__(self, size=0):
+        """Initializing the square class
+        Arguments is size. It represents the size of the square class
+        Raises:
+        - TypeError if size is not an integer
+        - ValueError if size is less than 0
+        """
 
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
-    self.__size = size
+        self.__size = size
 
     def area(self):
         """
