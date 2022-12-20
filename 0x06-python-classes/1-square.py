7aad53c7f9ca91ba4a15de3bb5a3513fd4e9cf57
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-20 10:38:58 -0800

    Initializing private object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Square:
+    """Represents a square class"""
+
+    def __init__(self, size):
+        """Initializing the square class
+        Argument is size. It represents the size of the square defined
+        """
+
+        self.__size = size
