d34fd8e9b1e4c651ed3657e4204209473d4312dd
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-21 03:39:12 -0800

    Property and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,16 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initializing the square class
         Arguments is size. It represents the size of the square class
-        Raises:
-        - TypeError if size is not an integer
-        - ValueError if size is less than 0
         """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
 
     @property
@@ -28,6 +19,12 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Setter for size
+        Raises:
+        - TypeError if size is not an integer
+        - ValueError if size is less than 0
+        """
+
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
@@ -35,9 +32,9 @@ class Square:
         self.__size = value
 
         def area(self):
-        """
-        Calculates area of the square
-        Returns: The square of the size
-        """
+            """
+            Calculates area of the square
+            Returns: The area of the square
+            """
 
-        return (self.__size ** 2)
+            return (self.__size ** 2)
