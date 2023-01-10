db2104c45f2d6ef04438c726fec46ad92e3b628d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-01-10 11:08:12 -0800

    Representing as a dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module that defines class to JSON function"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return.obj.__dict__
