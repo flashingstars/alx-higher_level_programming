d9b9cbf0213e0d02166bf1ab412bb5a10ccd44e2
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-01-10 11:02:16 -0800

    Saving arguments to a python list file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,14 @@
 import sys
 
 
-if __name__ == "__main__":
-    save_to_json = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+args = sys.argv[1:]
+jsonfile = "add_item.json"
+try:
+    my_list = load(jsonfile)
+except FileNotFoundError:
+    save(args, jsonfile)
+else:
+    my_list.extend(args)
+    save(my_list, jsonfile)
