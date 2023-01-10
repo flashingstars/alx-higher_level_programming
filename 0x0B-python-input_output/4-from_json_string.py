6a65e92b110b9bf33fbad082df1704fb54fa8b9a
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-01-10 10:27:51 -0800

    Object representation of a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that defines a JSON to object function"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object representation of a JSON string"""
+    return json.loads(my_str)
