ed779af688f257b42bb95f88150569d5ec65bf32
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-02-15 10:46:16 -0800

    Creating a primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- create an auto generated table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT AUTO_INCREMENT, PRIMARY KEY, NOT NULL
+	id INT AUTO_INCREMENT PRIMARY KEY
 	name VARCHAR(256) NOT NULL);
