7a420786fcf5e7be3e1e754e8dc1c321976487c3
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-02-15 11:05:18 -0800

    Creating a foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id)
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
