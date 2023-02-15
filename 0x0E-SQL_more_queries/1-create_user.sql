9c84ba670aaa94e3f309f38046029d5f68dfee14
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-02-15 12:06:27 -0800

    Permission to all users

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- create and grant permissions to a user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
+-- create and grant permissions to users
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
