d6b68aafbad3173af55709c593b624f675eb533d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-13 11:30:05 -0700

    Printing the 2nd argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const argc = process.argv[2];
-if (!argc){
-  console.log('No argument');}
-else{
-  console.log(argc);}
+if (!argc) {
+  console.log('No argument');
+} else {
+  console.log(argc);
+}
