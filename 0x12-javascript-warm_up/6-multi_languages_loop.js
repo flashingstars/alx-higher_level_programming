7428cc3d1470633529c2963ded459269597cea43
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-13 12:10:59 -0700

    for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let counter = 0; counter < myArray.length; counter++) {
+  console.log(myArray[counter]);
+}
