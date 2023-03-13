e976889316ed4db2ea473ae32126d48128ff6a43
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-13 11:23:36 -0700

    If and argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const argc = process.argv.length;
-if (argc < 3){
-	console.log('No argument');}
-else if (argc == 3){
-	console.log('Argument found');}
-else{
-	console.log('Arguments found');}
+if (argc < 3) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
