83ff9da69729615fd34106b6b319ecfed9a6a32d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-14 11:05:28 -0700

    Creating an instance method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 }
