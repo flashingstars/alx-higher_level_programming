bde5bf04bfa92ef2143b8dd08c2301943897ea8f
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-14 11:29:39 -0700

    Rotating and doubling

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,13 +13,16 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
+  }
 }
 module.exports = Rectangle;
