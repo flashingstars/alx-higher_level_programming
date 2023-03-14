219f5f77b4fb98a482ad56df315c503a2df5191a
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-14 10:59:40 -0700

    Creating an instance method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('x'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
