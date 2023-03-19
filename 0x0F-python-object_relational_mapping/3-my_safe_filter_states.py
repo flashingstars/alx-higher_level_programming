846e4959279f42146737af2a5a7ac99fa44b0159
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-19 12:22:47 -0700

    SQL injection fix

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" selecting with mysqldb """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("error connecting")
+    cur = connection.cursor()
+    try:
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY\
+        states.id", (sys.argv[4],))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error:
+        print("execution failed")
+    cur.close()
+    connection.close()
