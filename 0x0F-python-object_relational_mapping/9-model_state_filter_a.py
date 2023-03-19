8c4699a92e3a3100a200ea6066fcf6398c61b5f6
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-19 12:40:52 -0700

    Listing objects starting with a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" slqalchmy query """
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id)
+    results = query.all()
+    for state in results:
+        print(f"{state.id}: {state.name}")
+    session.close()
