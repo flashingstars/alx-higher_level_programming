76ddb22de930a86a10bcff6f434b581a5ac8a2b5
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-30 13:06:09 -0700

    POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #POST request with email and subject as variables sent to the URL displaying the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
