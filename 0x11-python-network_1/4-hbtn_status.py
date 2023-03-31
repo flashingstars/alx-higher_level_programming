52a83bfd9476ceaa3ebf8a700f371ab7c5dcf0e2
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-31 04:47:54 -0700

    Requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Using request to fetch https://alx-intranet.hbtn.io/status """
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status").text
+    print("body response:")
+    print(f'    - type: {type(response)}')
+    print(f'    - content: {response}')
